50e7619087c387656dbb90fb43c766d3b2447961
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2023-06-21 07:41:44 -0300

    Add files via upload

--- develop/nexus_experiments.R
+++ develop/nexus_experiments.R
@@ -14,8 +14,8 @@ devtools::install_github("cefet-rj-dal/event_datasets", force = TRUE, dep=FALSE,
 # Load packages
 library(daltoolbox)
 library(harbinger)
-#source("https://raw.githubusercontent.com/cefet-rj-dal/harbingerext/main/develop/nexus.R")
-source("E://Users//janio//Documents//Education//Mestrado e Doutorado//CEFET//2. Pesquisa//DAL_Events//harbingerNimbus//nexus.R")
+source("https://raw.githubusercontent.com/cefet-rj-dal/harbingerext/main/develop/nexus.R")
+#source("E://Users//janio//Documents//Education//Mestrado e Doutorado//CEFET//2. Pesquisa//DAL_Events//harbingerNimbus//nexus.R")
 library(dalevents)
 
 
@@ -40,24 +40,9 @@ names(data) <- c("series", "event")
 
 
 # Nexus -------------------------------------------------------------------
-
-##Under development
-#Probability of the observation being an event
-#detection$ef <- 0 #Events frequency of a series point
-#detection$bf <- 0 #Batch frequency of a series point
-#detection$pe <- detection$ef / detection$bf
-
-##Under development
-#Detection temporal lag
-#detection$first_batch <- 0 #First batch where the event was detected
-#detection$last_batch <- 0 #Last batch where the event was detected
-
 # Run Nexus ---------------------------------------------------------------
 run_nexus <- function(model, data, warm_size = 30, batch_size = 30, mem_batches = 0, png_folder="dev/plots/") {
   #require(tibble)
-  
-  #Empty list to store results across batches
-  hist <- list()
   
   #Create auxiliary batch and slide counters
   bt_num <- 1
@@ -70,25 +55,12 @@ run_nexus <- function(model, data, warm_size = 30, batch_size = 30, mem_batches 
   
   #Sliding batches through series
   while (!is.null(online_detector$datasource)) {
-    #Online detection
     online_detector <- detect(online_detector)
     
-    if (sld_bt == 1) {
-      ef <- 0
-    }
-    
-    #Update batch and slide counters and historical results
+    #Update batch and slide counters
     print(paste("Current position:", sld_bt+warm_size))
     sld_bt <- sld_bt + 1
-    
     if (sld_bt %% batch_size == 0) {
-      #Store result metrics parameters across batches - Historic
-      hist_sld <- tibble::tibble(batch = bt_num,
-                         idx = online_detector$detection$idx,
-                         ev = online_detector$detection$event)
-      
-      hist[[bt_num]] <- hist_sld
-      
       bt_num <- bt_num + 1
     }
     
@@ -101,29 +73,25 @@ run_nexus <- function(model, data, warm_size = 30, batch_size = 30, mem_batches 
     
   }
   
-  #Adding last batch results
-  hist_sld <- tibble::tibble(batch = bt_num,
-                     idx = online_detector$detection$idx,
-                     ev = online_detector$detection$event)
-  
-  hist[[bt_num]] <- hist_sld
-  
+  #Batch frequency of a time series point t
   online_detector$detection$bf <- ceiling((nrow(data)/batch_size)) - floor(online_detector$detection$idx/(batch_size)) #DONE
   
-  ev_idx <- which(online_detector$detection$event == 1) #Event indexes
+  #Event indexes
+  ev_idx <- which(online_detector$detection$event == 1)
   
+  #Event frequency of a time series point t
   online_detector$detection$ef <- 0
   online_detector$detection[ev_idx,]$ef <- ceiling((nrow(data)/batch_size)) - floor(online_detector$detection[ev_idx,]$idx/(batch_size)) ##CORRIGIR
   
-  online_detector$detection$pe <- online_detector$detection$ef / online_detector$detection$bf#DONE
+  #Event probability of a time series point t
+  online_detector$detection$pe <- online_detector$detection$ef / online_detector$detection$bf #DONE
   
-  online_detector$hist <- hist
   return(online_detector)
 }
 
 
 #Create and setup objects
-bt_size <- 100
+bt_size <- 10
 wm_size <- 30
 
 
@@ -152,29 +120,5 @@ print(evaluation$confMatrix)
 
 
 # ploting the results
-grf <- har_plot(result$detector, data$series,
-                      result$detection, data$event)
+grf <- har_plot(result$detector, data$series, result$detection, data$event)
 plot(grf)
-
-
-
-#View results across batches (example using Gecco Challenge complete ph series)
-View(result$hist[[1]])
-View(result$hist[[length(result$hist)/3]])
-View(result$hist[[length(result$hist)/2]])
-View(result$hist[[length(result$hist)]])
-
-out_file <- "result_exp2.RData"
-save(result, file = out_file, compress = TRUE)
-
-
-merged_result <- Reduce(
-  function(x, y, ...) merge(x, y, by = "idx", all = TRUE, ...),
-  result$hist
-)
-
-View(merged_result)
-
-out_file <- "merged_result_exp2.RData"
-save(merged_result, file = out_file, compress = TRUE)
- 
