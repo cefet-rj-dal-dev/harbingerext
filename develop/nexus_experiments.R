f7eef038929279df0da34c62f71d19b5b8e584e7
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2023-07-01 17:03:01 -0300

    Add files via upload

--- develop/nexus_experiments.R
+++ develop/nexus_experiments.R
@@ -1,5 +1,5 @@
 #############################################################################
-## Integration between Nexus + Harbinger + DALEvents
+## Integration between Nexus + Harbinger + DAL Events
 #############################################################################
 # install.packages("devtools")
 library(devtools)
@@ -15,7 +15,6 @@ devtools::install_github("cefet-rj-dal/event_datasets", force = TRUE, dep=FALSE,
 library(daltoolbox)
 library(harbinger)
 source("https://raw.githubusercontent.com/cefet-rj-dal/harbingerext/main/develop/nexus.R")
-#source("E://Users//janio//Documents//Education//Mestrado e Doutorado//CEFET//2. Pesquisa//DAL_Events//harbingerNimbus//nexus.R")
 library(dalevents)
 
 
@@ -45,6 +44,7 @@ run_nexus <- function(model, data, warm_size = 30, batch_size = 30, mem_batches 
   #Create auxiliary batch and slide counters
   bt_num <- 1
   sld_bt <- 1
+  ef_start <- FALSE
   
   #Prepare data to experiment
   datasource <- nex_simulated_datasource("data", data$series)
@@ -58,7 +58,22 @@ run_nexus <- function(model, data, warm_size = 30, batch_size = 30, mem_batches 
     #Update batch and slide counters
     print(paste("Current position:", sld_bt+warm_size))
     sld_bt <- sld_bt + 1
+    
     if (sld_bt %% batch_size == 0) {
+      
+      if (!ef_start) {
+        parc <- online_detector$detection[which(online_detector$detection$event == 1),]
+        parc$ef <- 1
+        ef_start <- TRUE
+      } else {
+        temp <- online_detector$detection[which(online_detector$detection$event == 1),]
+        if (!is.null(temp)) {
+          parc <- merge(temp, parc, all = TRUE)
+          parc$ef[is.na(parc$ef)] <- 0
+          parc$ef[which(parc$event == 1)] <- parc$ef[which(parc$event == 1)] + 1
+        }
+        print('...')
+      }
       bt_num <- bt_num + 1
     }
     
@@ -71,37 +86,67 @@ run_nexus <- function(model, data, warm_size = 30, batch_size = 30, mem_batches 
     
   }
   
+  ##EVENT PROBABILITY
+  #Last batch update
+  temp <- online_detector$detection[which(online_detector$detection$event == 1),]
+  if (!is.null(temp)) {
+    parc <- merge(temp, parc, all = TRUE)
+    parc$ef[is.na(parc$ef)] <- 0
+    parc$ef[which(parc$event == 1)] <- parc$ef[which(parc$event == 1)] + 1
+  }
+  
   #Batch frequency of a time series point t
-  online_detector$detection$bf <- ceiling((nrow(data)/batch_size)) - floor(online_detector$detection$idx/(batch_size)) #DONE
-  
-  #Event indexes
-  ev_idx <- which(online_detector$detection$event == 1)
-  
-  #Event frequency of a time series point t
-  online_detector$detection$ef <- 0
-  online_detector$detection[ev_idx,]$ef <- ceiling((nrow(data)/batch_size)) - floor(online_detector$detection[ev_idx,]$idx/(batch_size)) ##CORRIGIR
+  parc$bf <- ceiling((nrow(data)/batch_size)) - floor(parc$idx/(batch_size))
   
   #Event probability of a time series point t
-  online_detector$detection$pe <- online_detector$detection$ef / online_detector$detection$bf #DONE
+  parc$pe <- parc$ef / parc$bf
   
+  online_detector$prob <- parc
   return(online_detector)
 }
 
 
 #Create and setup objects
-bt_size <- 10
-wm_size <- 30
-
+bt_size <- c(1,3,9,27,81)
+wm_size <- c(1,3,9,27,81)
 
 # establishing method
-model <- hanr_fbiad()
+#FBI-AD - ARIMA - CF - LSTM? - RN com regressão
+#https://nbviewer.org/github/cefet-rj-dal/harbinger-examples/blob/main/anomalies/hanr_ml_lstm.ipynb
+model <- hanr_fbiad() #FBI-AD
+model <- hanr_arima() #ARIMA
+model <- hcp_cf_arima() #CF using Arima - PROBLEM
+model <- hcp_cf_lr() #CF using Linear Regression - PROBLEM
+model <- hanr_garch() #GARCH
+model <- hanr_ml() #ML Linear Regression
 
+library("reticulate")
+source("https://raw.githubusercontent.com/cefet-rj-dal/daltoolbox-examples/main/ts_tlstm.R")
+reticulate::source_python("https://raw.githubusercontent.com/cefet-rj-dal/daltoolbox-examples/main/ts_tlstm.py")
+model <- hanr_ml(ts_tlstm(ts_gminmax(), input_size=4, epochs=10000)) #LSTM
 
-result <- run_nexus(model=model, data=data, warm_size=wm_size, batch_size=bt_size, mem_batches=0, png_folder="dev/plots/")
+## --------------------------------------------------------
+result <- run_nexus(model=model, data=data, warm_size=wm_size[5], batch_size=bt_size[5], mem_batches=0, png_folder="dev/plots/")
 View(result$detection)
+View(result$prob)
+
+prob <- result$prob
+save(prob, file = "~/janio/harbinger/dev/prob_ph_81.RData")
+
+
+#Filter by limit
+plim = 0.9999
+prob_lim <- subset(prob, pe > plim)
+
+det_prob <- result$detection
+det_prob$event <- 0
+det_prob$event[which(det_prob$idx %in% prob_lim$idx)] <- 1
+
+View(det_prob)
 
 #Sum of events
 sum(result$detection$event)
+sum(det_prob$event)
 
 #Head of detections
 ev_idx <- which(result$detection$event == 1)
@@ -117,6 +162,16 @@ evaluation <- evaluate(result$detector,
 print(evaluation$confMatrix)
 
 
-# ploting the results
+#Evaluate limit query detection
+print(evaluate(result$detector, det_prob$event, data$event)$confMatrix)
+
+
+
+# plotting the results
 grf <- har_plot(result$detector, data$series, result$detection, data$event)
 plot(grf)
+
+
+# plotting limit query detection
+grf_lim <- har_plot(result$detector, data$series, det_prob, data$event)
+plot(grf_lim)
