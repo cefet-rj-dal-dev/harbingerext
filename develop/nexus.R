d66f4e34623c38c3e9c8b20ce3747f5064171f6a
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2023-10-14 18:21:20 -0300

    Add files via upload

--- develop/nexus.R
+++ develop/nexus.R
@@ -117,28 +117,28 @@ detect.nexus <- function(obj) {
 
 # Run Nexus ---------------------------------------------------------------
 run_nexus <- function(model, data, warm_size = 30, batch_size = 30, mem_batches = 0) {
-  #Create auxiliary batch and slide counters
   bt_num <- 1
   sld_bt <- 1
   exec_time <- c()
   ef_start <- FALSE
+  deb = FALSE
   
   datasource <- nex_simulated_datasource("data", data$series)
   online_detector <- nexus(datasource, model, warm_size = warm_size, batch_size = batch_size, mem_batches = mem_batches)
   online_detector <- warmup(online_detector)
   
-  #Sliding batches through series
+  print("Starting streaming analysis...")
   start_time <- Sys.time()
   while (!is.null(online_detector$datasource)) {
     online_detector <- detect(online_detector)
-    #Update batch and slide counters
-    print(paste("Current position:", sld_bt+warm_size))
-    sld_bt <- sld_bt + 1
+    if (deb) {
+      print(paste("Current position:", sld_bt+warm_size))
+    }
     
+    sld_bt <- sld_bt + 1
     if (sld_bt %% batch_size == 0) {
       exec_time <- append(exec_time, as.numeric(difftime(Sys.time(),start_time, units = "secs")))
       if (!ef_start) {
-        #First batch annotation
         parc <- online_detector$detection[which(online_detector$detection$event == 1),]
         if(nrow(parc) != 0) {
           parc$ef <- 1
@@ -149,64 +149,60 @@ run_nexus <- function(model, data, warm_size = 30, batch_size = 30, mem_batches 
       } else {
         temp <- online_detector$detection[which(online_detector$detection$event == 1),]
         if (!is.null(temp)) {
-          #Event frequency counter
           parc <- merge(temp, parc, all = TRUE)
           parc$ef[is.na(parc$ef)] <- 0
           parc$ef[which(parc$event == 1)] <- parc$ef[which(parc$event == 1)] + 1
-          #First batch annotation
+          
           parc$fdb[is.na(parc$fdb)] <- 0
           parc$fdb[which(parc$ef == 1)] <- bt_num
         }
       }
-      #Batch number update
+      
       bt_num <- bt_num + 1
     }
-    #Print partial results
-    print("Results:")
-    print(table(online_detector$detection$event))
-    print("--------------------------")
-    print(paste("Batch:", bt_num))
-    print("==========================")
+    if (deb) {
+      #Print partial results in debug mode
+      print("Results:")
+      print(table(online_detector$detection$event))
+      print("--------------------------")
+      print(paste("Batch:", bt_num))
+      print("==========================")
+    }
   }
   
-  #Last batch execution time record
   exec_time <- append(exec_time, as.numeric(difftime(Sys.time(),start_time, units = "secs")))
-  #Last batch update
+  
   temp <- online_detector$detection[which(online_detector$detection$event == 1),]
   if (!is.null(temp)) {
     parc <- merge(temp, parc, all = TRUE)
     parc$ef[is.na(parc$ef)] <- 0
     parc$ef[which(parc$event == 1)] <- parc$ef[which(parc$event == 1)] + 1
   }
-  #First batch annotation
+  
   parc$fdb[is.na(parc$fdb)] <- 0
   parc$fdb[which(parc$ef == 1)] <- bt_num
-  #Batch frequency of a time series point t
+  
   parc$bf <- ceiling((nrow(data)/batch_size)) - floor(parc$idx/(batch_size))
-  #Event probability of a time series point t
+  
   parc$pe <- parc$ef / parc$bf
   online_detector$prob <- parc[,c(1,5,4,6,7)]
   online_detector$time <- exec_time
+  print("Nexus run complete.")
   return(online_detector)
 }
 
 
 lag_evaluate <- function(pos, nexus_result, reference) {
-  #Result info collect
   t_res_info <- nexus_result$prob[nexus_result$prob$idx == pos,]
   wsz <- nexus_result$warm_size
   bsz <- nexus_result$batch_size
-  #Formula parameters
   sb_t <- ceiling(pos / (bsz+wsz))
   
   if (nrow(t_res_info) > 0) {
-    #Formula parameters
     fdb_t <- t_res_info$fdb
-    #Lag calculation
     lag_t = fdb_t - sb_t
-    #Return lag analysis
+    
     return(data.frame(idx = pos, fdb = fdb_t, sb = sb_t, lag = lag_t, batch_size = bsz, method = class(nexus_result$detector)[1]))
   }
-  #Return info about not lag available
   return(data.frame(idx = pos, fdb = NA, sb = sb_t, lag = NA, batch_size = bsz, method = class(nexus_result$detector)[1]))
 }
